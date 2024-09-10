6a51c2fb8cf1de757d69fbed0a7f4ec1e5e09ea0
Evan Duhon <evangduhon@icloud.com>
2024-09-10 13:22:15 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
 
   dataShapes.push(shape);
   // TODO 2: add a new property to all data shapes
-  for(var i; i < dataShapes.length; i++){
+  for(var i = 0; i < dataShapes.length; i++){
     var currentShape = dataShapes[i]
 
     if(currentShape.color === 'red'){
@@ -70,10 +70,17 @@ $(document).ready(function () {
   }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color,shape,repeat){
+   setBackgroundWithSimple(color,shape,repeat)
+   animationDetails.displayType = 2 
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad(data,repeat){
+    repeat = repeat + 1
+    setBackgroundWithMixed(data,repeat)
+    animationDetails.displayType = 3
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -86,12 +93,15 @@ $(document).ready(function () {
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex]
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+   var currentShape = dataShapes[currentIndex]
+   var repeat = currentShape.repeat
+   handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
